124f07c0b90d35328dd0091714163e5dae1449bb
Ammie Tea <ammie.tea@getquip.com>
2024-12-16 11:19:58 -0500

    stage recharge

--- quip_data/models/stage/recharge/stg_recharge__events.sql
+++ quip_data/models/stage/recharge/stg_recharge__events.sql
@@ -1,23 +1,25 @@
-WITH source AS (
-	SELECT * FROM {{ source('recharge', 'events') }}
-)
+-- WITH source AS (
+-- 	SELECT * FROM {{ source('recharge', 'events') }}
+-- )
 
-SELECT 
-	id
-	, customer_id
-	, object_id
-	, created_at
-	, `custom_attributes`[SAFE_OFFSET(0)].`key`
-	, `custom_attributes`[SAFE_OFFSET(0)].value
-	, description, object_type
-	, `source`.account_id
-	, `source`.api_token_id
-	, `source`.account_email
-	, `source`.api_token_name
-	, `source`.origin
-	, `source`.user_type
-	, `updated_attributes`[SAFE_OFFSET(0)].attribute
-	, `updated_attributes`[SAFE_OFFSET(0)].previous_value
-	, `updated_attributes`[SAFE_OFFSET(0)].value
-	, verb 
-FROM source
+-- SELECT 
+-- 	id
+-- 	, customer_id
+-- 	, object_id
+-- 	, created_at
+-- 	, `custom_attributes`[SAFE_OFFSET(0)].`key`
+-- 	, `custom_attributes`[SAFE_OFFSET(0)].value
+-- 	, description, object_type
+-- 	, `source`.account_id
+-- 	, `source`.api_token_id
+-- 	, `source`.account_email
+-- 	, `source`.api_token_name
+-- 	, `source`.origin
+-- 	, `source`.user_type
+-- 	, `updated_attributes`[SAFE_OFFSET(0)].attribute
+-- 	, `updated_attributes`[SAFE_OFFSET(0)].previous_value
+-- 	, `updated_attributes`[SAFE_OFFSET(0)].value
+-- 	, verb 
+-- FROM source
+
+SELECT "pass" AS pass
