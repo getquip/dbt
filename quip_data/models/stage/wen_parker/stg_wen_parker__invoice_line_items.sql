21c361f37f68b1e3736c7f666f430c0e2e49dede
Ammie Tea <ammie.tea@getquip.com>
2025-01-15 10:49:07 -0500

    create stage models

---
+++ quip_data/models/stage/wen_parker/stg_wen_parker__invoice_line_items.sql
@@ -0,0 +1,19 @@
+WITH source AS (
+	SELECT * FROM {{ source('wen_parker', 'invoice_line_items') }}
+)
+/*
+	This model MUST be deduplicated by unique records. This is because each file contains data for
+	the last 90 days, and the same invoice may appear in multiple files.
+*/
+SELECT DISTINCT
+  invoice_number
+  , PARSE_DATE('%Y%m%d', invoice_date) AS invoice_date
+  , payer_code
+  , payer_name
+  , TRIM(LOWER(shipment_type)) AS shipment_type
+  , house_bill_number
+  , charge_code
+  , charge_name
+  , invoice_currency
+  , invoice_amount
+FROM source
