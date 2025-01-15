3bf336e8140269b41c178a3db709638150d3df8b
Ammie Tea <ammie.tea@getquip.com>
2025-01-15 15:06:47 -0500

    update dedupe logic

--- quip_data/models/stage/wen_parker/stg_wen_parker__invoice_line_items.sql
+++ quip_data/models/stage/wen_parker/stg_wen_parker__invoice_line_items.sql
@@ -1,19 +1,32 @@
 WITH source AS (
 	SELECT * FROM {{ source('wen_parker', 'invoice_line_items') }}
 )
+, cleaned AS (
+  SELECT
+    {{ dbt_utils.generate_surrogate_key([
+      'invoice_number'
+      , 'house_bill_number'
+      , 'charge_code'
+      , 'invoice_date'
+      , 'shipment_type'
+    ])}} AS invoice_line_item_id
+    , invoice_number
+    , PARSE_DATE('%Y%m%d', invoice_date) AS invoice_date
+    , payer_code
+    , payer_name
+    , TRIM(LOWER(shipment_type)) AS shipment_type
+    , house_bill_number
+    , charge_code
+    , charge_name
+    , invoice_currency
+    , CAST(REPLACE(REPLACE(invoice_amount, ',',''), '$','') AS FLOAT64) AS invoice_amount
+    , source_synced_at
+  FROM source
+)
+
 /*
 	This model MUST be deduplicated by unique records. This is because each file contains data for
 	the last 90 days, and the same invoice may appear in multiple files.
 */
-SELECT DISTINCT
-  invoice_number
-  , PARSE_DATE('%Y%m%d', invoice_date) AS invoice_date
-  , payer_code
-  , payer_name
-  , TRIM(LOWER(shipment_type)) AS shipment_type
-  , house_bill_number
-  , charge_code
-  , charge_name
-  , invoice_currency
-  , CAST(REPLACE(REPLACE(invoice_amount, ',',''), '$','') AS FLOAT64) AS invoice_amount
-FROM source
+SELECT * FROM cleaned
+QUALIFY ROW_NUMBER() OVER(PARTITION BY invoice_line_item_id ORDER BY source_synced_at DESC) = 1
