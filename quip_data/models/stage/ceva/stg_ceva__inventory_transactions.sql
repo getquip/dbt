454775d325cac3de956bdece9533cc94d78d22c1
Ammie Tea <ammie.tea@getquip.com>
2025-01-23 13:46:17 -0500

    add yaml and job configs for recharge

---
+++ quip_data/models/stage/ceva/stg_ceva__inventory_transactions.sql
@@ -0,0 +1,40 @@
+WITH source AS (
+    SELECT * FROM {{ source('ceva', 'inventory_transactions') }}
+)
+
+, cleaned AS (
+    SELECT
+        site_id AS warehouse_id
+        , 'Fontana, CA' AS warehouse_name
+        , sku_id AS sku
+        , `key` AS insert_id
+        , description
+        , update_qty AS quantity
+        , reference_id AS shipment_id
+        , purchase_order AS manifest_po
+        , reason_id AS inventory_reason_id
+        , reason_desc AS inventory_reason
+        , batch_id
+        , source_file_name
+        , source_synced_at
+        , 'ceva' AS provider
+        , DATE(to_date) AS to_date
+        , DATE(from_date) AS from_date
+        , TIMESTAMP(complete_dstamp) AS completed_timestamp
+        , LOWER(code) AS code
+    FROM source
+)
+
+SELECT
+    *
+    , CASE
+        WHEN code = 'shipment' THEN 'ship'
+        WHEN code = 'adjustment' THEN 'adjust'
+        ELSE REGEXP_REPLACE(code , r'\s' , '_')
+    END AS type
+    /*
+    quantity is negative in Newgistics when it's a deduction.
+    CEVA reports update_qty as a positive number for Shipment, this flips that
+    */
+    , IF(code = 'shipment' , -quantity , quantity) AS quantity
+FROM cleaned
