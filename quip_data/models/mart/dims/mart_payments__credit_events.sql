5ce490d125b57e350e1e0d63f484ec4c77971d08
Dipali Mistri <dipalim06@gmail.com>
2025-03-03 12:01:02 -0800

    message

--- quip_data/models/mart/dims/mart_payments__credit_events.sql
+++ quip_data/models/mart/dims/mart_payments__credit_events.sql
@@ -1,34 +1,16 @@
 
-WITH first_credit AS (
-	-- identify first credit associated with a credit_account_id
+WITH first_credit_debit AS (
 	SELECT
-		credit_event_id
-		, credit_account_id
-		, created_at
+	credit_account_id
+	, MIN(IF(payment_transaction_type = 'credit', created_at, NULL)) AS first_credit
+	, MIN(IF(payment_transaction_type = 'debit', created_at, NULL)) AS first_debit
 	FROM {{ ref('int_fct_payments__credit_events') }}
-	WHERE payment_transaction_type = 'credit'
-	QUALIFY ROW_NUMBER () OVER (PARTITION BY credit_account_id ORDER BY created_at) = 1
-)
-
-, first_debit AS (
-	-- identify first debit associated with a credit_account_id
-	SELECT
-		credit_event_id
-		, credit_account_id
-		, created_at
-	FROM {{ ref('int_fct_payments__credit_events') }}
-	WHERE payment_transaction_type = 'debit'
-	QUALIFY ROW_NUMBER () OVER (PARTITION BY credit_account_id ORDER BY created_at) = 1
+	GROUP BY 1
 )
 
 SELECT
     e.*
-    , CASE 
-		WHEN payment_transaction_type = 'debit' 
-			THEN TIMESTAMP_DIFF(d.created_at, c.created_at, DAY) 
-		END AS days_between_credit_and_debit
+    , TIMESTAMP_DIFF(f.first_debit, f.first_credit, DAY) AS days_between_credit_and_debit
 FROM {{ ref('int_fct_payments__credit_events') }} e
-LEFT JOIN first_credit c
-    ON e.credit_account_id = c.credit_account_id
-LEFT JOIN first_debit d
-	ON e.credit_account_id = d.credit_account_id
+LEFT JOIN first_credit_debit f
+    ON e.credit_account_id = f.credit_account_id
