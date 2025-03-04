6b42cb9a1264f3e9d91f686b09e4ec46903f756a
Dipali Mistri <dipalim06@gmail.com>
2025-03-04 10:15:54 -0800

    -

---
+++ quip_data/models/mart/dims/credit_account_dims.sql
@@ -0,0 +1,7 @@
+
+SELECT
+credit_account_id
+, MIN(IF(payment_transaction_type = 'credit', created_at, NULL)) AS first_credit
+, MIN(IF(payment_transaction_type = 'debit', created_at, NULL)) AS first_debit
+FROM {{ ref('int_fct_payments__credit_events') }}
+GROUP BY 1
