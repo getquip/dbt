453a6330c8d13e4091b907666d049eac3f08daa9
Dipali Mistri <dipalim06@gmail.com>
2025-03-04 11:51:55 -0800

    -

--- quip_data/models/mart/dims/credit_account_dims.sql
+++ quip_data/models/mart/dims/credit_account_dims.sql
@@ -1,7 +1,16 @@
 
-SELECT
+WITH base AS (
+
+	SELECT
+	credit_account_id
+	, MIN(IF(payment_transaction_type = 'credit', created_at, NULL)) AS first_credit
+	, MIN(IF(payment_transaction_type = 'debit', created_at, NULL)) AS first_debit
+	FROM {{ ref('int_fct_payments__credit_events') }}
+	GROUP BY 1
+
+)
+
+SELECT 
 credit_account_id
-, MIN(IF(payment_transaction_type = 'credit', created_at, NULL)) AS first_credit
-, MIN(IF(payment_transaction_type = 'debit', created_at, NULL)) AS first_debit
-FROM {{ ref('int_fct_payments__credit_events') }}
-GROUP BY 1
+, TIMESTAMP_DIFF(f.first_debit, f.first_credit, DAY) AS days_between_credit_and_debit
+FROM base
