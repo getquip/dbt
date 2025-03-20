65ed5cd961737e0ff24a229f20853b3c5d07f7ca
Ammie Tea <ammie.tea@getquip.com>
2025-03-19 21:31:46 -0400

    add lookback window arg

--- quip_data/macros/get_max_partition.sql
+++ quip_data/macros/get_max_partition.sql
@@ -1,7 +1,12 @@
-{% macro get_max_partition(partition_field, where_clause=None) %}
+{% macro get_max_partition(partition_field, where_clause=None, lookback_window=None) %}
     -- Construct the SQL query to get the MAX value of the partition field
     {%- set query %}
-        SELECT MAX({{ partition_field }}) AS max_partition
+        SELECT 
+            {% if lookback_window %}
+                DATE_SUB(MAX({{ partition_field }}), INTERVAL {{ lookback_window}} DAY) AS max_partition
+            {% else %}
+                MAX({{ partition_field }}) AS max_partition
+            {% endif %}
         FROM {{ this }}
         {% if where_clause %}
             WHERE {{ where_clause }}  -- Add the WHERE clause if provided
