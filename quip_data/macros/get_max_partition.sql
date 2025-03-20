fdf9e7e005453474d83f41050a7a0c849b136c8b
Ammie Tea <ammie.tea@getquip.com>
2025-03-19 21:14:46 -0400

    create macro

--- quip_data/macros/get_max_partition.sql
+++ quip_data/macros/get_max_partition.sql
@@ -1,18 +1,21 @@
 {% macro get_max_partition(partition_field, where_clause=None) %}
-    {% set query%}
-		SELECT MAX({{ partition_field }})
-		FROM {{ this }}
-		{% if where_clause %}
-			WHERE {{ where_clause }}
-		{% endif %}
-	{% endset %}
-	
-	{% set result = run_query(sql) %}
-	
+    -- Construct the SQL query to get the MAX value of the partition field
+    {%- set query %}
+        SELECT MAX({{ partition_field }}) AS max_partition
+        FROM {{ this }}
+        {% if where_clause %}
+            WHERE {{ where_clause }}  -- Add the WHERE clause if provided
+        {% endif %}
+    {% endset %}
+
+    --Execute the SQL query using dbt's run_query function
+    {% set result = run_query(query) %}
+    
+    -- Check if the query returned a result
     {% if result %}
+        -- Extract the max value from the first column of the first row in the result set
         {% set max_value = result.columns[0].values()[0] %}
-        {{ return(max_value) }}
-    {% else %}
-        {{ return(None) }}
+		{% do log('Last partition: ' ~ max_value, info=True) %}
+		{{ return(max_value) }}  -- Return the max value as a string
     {% endif %}
 {% endmacro %}
