8495c263dce8a7291a78c9a85cb39221167c3bca
Ammie Tea <ammie.tea@getquip.com>
2025-03-23 12:51:13 -0400

    stage rudderstack

--- quip_data/models/stage/rudderstack/stg_rudderstack__order_created.sql
+++ quip_data/models/stage/rudderstack/stg_rudderstack__order_created.sql
@@ -5,16 +5,17 @@ source AS (
 )
 
 , historical__littledata AS (
-	SELECT * FROM {{ source("segment", "order_completed") }}
+	SELECT * FROM {{ source("segment", "littledata__order_completed") }}
 )
 
 , historical__legacy AS (
-	SELECT * FROM {{ source("segment", "legacy__identifies") }}
+	SELECT * FROM {{ source("segment", "legacy__order_completed") }}
 )
 
 -------------------------------------------------------
 ----------------- FINISH REFERENCES -------------------
 -------------------------------------------------------
+
 , cleaned AS (
 	SELECT
 		"rudderstack" AS source_name
@@ -33,10 +34,6 @@ source AS (
 		, confirmation_number
 		, confirmed AS is_confirmed
 		, contact_email
-		, context_cart_token
-		, context_checkout_token
-		, context_destination_id
-		, context_destination_type
 		, context_integration_name
 		, context_ip
 		, context_library_name
@@ -44,35 +41,15 @@ source AS (
 		, context_request_ip
 		, context_session_id
 		, context_source_id
-		, context_source_type
+		, LOWER(context_source_type) AS context_source_type
 		, context_topic
 		, created_at
 		, currency
-		, current_subtotal_price
-		, current_subtotal_price_set_presentment_money_amount
-		, current_subtotal_price_set_presentment_money_currency_code
-		, current_subtotal_price_set_shop_money_amount
-		, current_subtotal_price_set_shop_money_currency_code
-		, current_total_discounts
-		, current_total_discounts_set_presentment_money_amount
-		, current_total_discounts_set_presentment_money_currency_code
-		, current_total_discounts_set_shop_money_amount
-		, current_total_discounts_set_shop_money_currency_code
-		, current_total_price
-		, current_total_price_set_presentment_money_amount
-		, current_total_price_set_presentment_money_currency_code
-		, current_total_price_set_shop_money_amount
-		, current_total_price_set_shop_money_currency_code
-		, current_total_tax
-		, current_total_tax_set_presentment_money_amount
-		, current_total_tax_set_presentment_money_currency_code
-		, current_total_tax_set_shop_money_amount
-		, current_total_tax_set_shop_money_currency_code
 		, customer_locale
 		, discount_applications
 		, discount_codes
 		, email
-		, estimated_taxes
+		, estimated_taxes AS has_estimated_taxes
 		, `event` AS event_name
 		, event_text
 		, financial_status
@@ -106,41 +83,21 @@ source AS (
 		, sent_at
 		, shipping_lines
 		, source_name AS channel
-		, subtotal_price
-		, subtotal_price_set_presentment_money_amount
-		, subtotal_price_set_presentment_money_currency_code
-		, subtotal_price_set_shop_money_amount
-		, subtotal_price_set_shop_money_currency_code
+		, SAFE_CAST(subtotal_price AS NUMERIC) AS subtotal_price
 		, tags
-		, tax
+		, SAFE_CAST(tax AS NUMERIC) AS tax
 		, tax_exempt
 		, tax_lines
 		, taxes_included
 		, `timestamp`
 		, token
-		, total_discounts
-		, total_discounts_set_presentment_money_amount
-		, total_discounts_set_presentment_money_currency_code
-		, total_discounts_set_shop_money_amount
-		, total_discounts_set_shop_money_currency_code
-		, total_line_items_price
-		, total_line_items_price_set_presentment_money_amount
-		, total_line_items_price_set_presentment_money_currency_code
-		, total_line_items_price_set_shop_money_amount
-		, total_line_items_price_set_shop_money_currency_code
+		, COALESCE(SAFE_CAST(total_discounts AS NUMERIC), 0) AS total_discounts
+		, COALESCE(SAFE_CAST(total_line_items_price AS NUMERIC), 0) AS total_line_items_price
 		, total_outstanding
-		, total_price_set_presentment_money_amount
-		, total_price_set_presentment_money_currency_code
 		, total_price_set_shop_money_amount
-		, total_price_set_shop_money_currency_code
-		, total_shipping_price_set_presentment_money_amount
-		, total_shipping_price_set_presentment_money_currency_code
-		, total_shipping_price_set_shop_money_amount
-		, total_shipping_price_set_shop_money_currency_code
+		, SAFE_CAST(total_shipping_price_set_presentment_money_amount AS NUMERIC) AS total_shipping_price_set_presentment_money_amount
 		, total_tax_set_presentment_money_amount
-		, total_tax_set_presentment_money_currency_code
 		, total_tax_set_shop_money_amount
-		, total_tax_set_shop_money_currency_code
 		, total_tip_received
 		, total_weight
 		, updated_at
@@ -154,134 +111,185 @@ source AS (
 
 	SELECT
 		"littledata" AS source_name
-		, AS admin_graphql_api_id
+		, context_google_analytics_client_id AS admin_graphql_api_id
 		, anonymous_id
-		, AS app_id
-		, AS browser_ip
-		, AS buyer_accepts_marketing
-		, AS cart_token
+		, app_id
+		, NULL AS browser_ip
+		, context_consent_category_preferences_advertising AS buyer_accepts_marketing
+		, cart_id AS cart_token
 		, checkout_id
-		, AS checkout_token
-		, AS client_details_accept_language
-		, AS client_details_browser_ip
-		, AS client_details_user_agent
-		, AS closed_at
-		, AS confirmation_number
-		, AS is_confirmed
-		, AS contact_email
-		, AS context_cart_token
-		, AS context_checkout_token
-		, AS context_destination_id
-		, AS context_destination_type
-		, AS context_integration_name
-		, AS context_ip
-		, AS context_library_name
-		, AS context_library_version
-		, AS context_request_ip
-		, AS context_session_id
-		, AS context_source_id
-		, AS context_source_type
-		, AS context_topic
-		, AS created_at
-		, AS currency
-		, AS current_subtotal_price
-		, AS current_subtotal_price_set_presentment_money_amount
-		, AS current_subtotal_price_set_presentment_money_currency_code
-		, AS current_subtotal_price_set_shop_money_amount
-		, AS current_subtotal_price_set_shop_money_currency_code
-		, AS current_total_discounts
-		, AS current_total_discounts_set_presentment_money_amount
-		, AS current_total_discounts_set_presentment_money_currency_code
-		, AS current_total_discounts_set_shop_money_amount
-		, AS current_total_discounts_set_shop_money_currency_code
-		, AS current_total_price
-		, AS current_total_price_set_presentment_money_amount
-		, AS current_total_price_set_presentment_money_currency_code
-		, AS current_total_price_set_shop_money_amount
-		, AS current_total_price_set_shop_money_currency_code
-		, AS current_total_tax
-		, AS current_total_tax_set_presentment_money_amount
-		, AS current_total_tax_set_presentment_money_currency_code
-		, AS current_total_tax_set_shop_money_amount
-		, AS current_total_tax_set_shop_money_currency_code
-		, AS customer_locale
-		, AS discount_applications
-		, AS discount_codes
-		, AS email
-		, AS estimated_taxes
+		, NULL AS checkout_token
+		, NULL AS client_details_accept_language
+		, NULL AS client_details_browser_ip
+		, NULL AS client_details_user_agent
+		, NULL AS closed_at
+		, NULL AS confirmation_number
+		, NULL AS is_confirmed
+		, context_traits_email AS contact_email
+		, context_integration_name
+		, context_ip
+		, context_library_name
+		, context_library_version
+		, NULL AS context_request_ip
+		, context_google_analytics_session_id AS context_session_id
+		, NULL AS context_source_id
+		, LOWER(source_name) AS context_source_type
+		, action_source AS context_topic
+		, sent_at AS created_at
+		, currency
+		, NULL AS customer_locale
+		, NULL AS discount_applications
+		, coupon AS discount_codes
+		, email
+		, NULL AS has_estimated_taxes
 		, `event` AS event_name
-		, AS event_text
-		, AS financial_status
-		, AS fulfillment_status
-		, AS fulfillments
-		, AS id
-		, AS landing_site
-		, AS landing_site_ref
+		, event_text
+		, 'paid' AS financial_status -- assuming these are paid as this event is 'order_completed` whereas the rudderstack event is `order_created`
+		, fulfillment_status
+		, NULL AS fulfillments
+		, NULL AS order_created_id
+		, NULL AS landing_site
+		, NULL AS landing_site_ref
 		, loaded_at
-		, AS location_id
-		, AS `name`
-		, AS note
-		, AS note_attributes
-		, AS `number`
-		, shopify_order_id AS order_id
-		, AS order_number
-		, AS order_status_url
+		, NULL AS location_id
+		, NULL AS `name`
+		, NULL AS note
+		, NULL AS note_attributes
+		, NULL AS `number`
+		, NULL AS order_id
+		, NULL AS order_number
+		, NULL AS order_status_url
 		, original_timestamp
 		, payment_gateway_littledata AS payment_gateway_names
-		, AS payment_terms_created_at
-		, AS payment_terms_id
-		, AS payment_terms_payment_terms_name
-		, AS payment_terms_payment_terms_type
-		, AS payment_terms_updated_at
+		, NULL AS payment_terms_created_at
+		, NULL AS payment_terms_id
+		, NULL AS payment_terms_payment_terms_name
+		, NULL AS payment_terms_payment_terms_type
+		, NULL AS payment_terms_updated_at
 		, context_traits_phone AS phone
 		, presentment_currency
-		, AS processed_at
-		, AS products
+		, NULL AS processed_at
+		, products
 		, received_at
-		, AS referring_site
+		, context_page_referrer AS referring_site
 		, sent_at
-		, AS shipping_lines
-		, AS channel
+		, NULL AS shipping_lines
+		, NULL AS channel
 		, subtotal AS subtotal_price
-		, AS subtotal_price_set_presentment_money_amount
-		, AS subtotal_price_set_presentment_money_currency_code
-		, AS subtotal_price_set_shop_money_amount
-		, AS subtotal_price_set_shop_money_currency_code
-		, AS tags
-		, tax
-		, AS tax_exempt
-		, AS tax_lines
-		, AS taxes_included
-		, AS `timestamp`
-		, AS token
-		, AS total_discounts
-		, AS total_discounts_set_presentment_money_amount
-		, AS total_discounts_set_presentment_money_currency_code
-		, AS total_discounts_set_shop_money_amount
-		, AS total_discounts_set_shop_money_currency_code
-		, AS total_line_items_price
-		, AS total_line_items_price_set_presentment_money_amount
-		, AS total_line_items_price_set_presentment_money_currency_code
-		, AS total_line_items_price_set_shop_money_amount
-		, AS total_line_items_price_set_shop_money_currency_code
-		, AS total_outstanding
-		, AS total_price_set_presentment_money_amount
-		, AS total_price_set_presentment_money_currency_code
-		, AS total_price_set_shop_money_amount
-		, AS total_price_set_shop_money_currency_code
-		, AS total_shipping_price_set_presentment_money_amount
-		, AS total_shipping_price_set_presentment_money_currency_code
-		, AS total_shipping_price_set_shop_money_amount
-		, AS total_shipping_price_set_shop_money_currency_code
-		, AS total_tax_set_presentment_money_amount
-		, AS total_tax_set_presentment_money_currency_code
-		, AS total_tax_set_shop_money_amount
-		, AS total_tax_set_shop_money_currency_code
-		, AS total_tip_received
-		, AS total_weight
-		, AS updated_at
+		, NULL AS tags
+		, COALESCE(CAST(tax AS NUMERIC), 0) AS tax
+		, NULL AS tax_exempt
+		, NULL AS tax_lines
+		, NULL AS taxes_included
+		, `timestamp`
+		, NULL AS token
+		, COALESCE(CAST(discount AS NUMERIC), 0) AS total_discounts
+		, COALESCE(CAST(total AS NUMERIC), 0) AS total_line_items_price
+		, NULL AS total_outstanding
+		, NULL AS total_price_set_shop_money_amount
+		, CAST(shipping AS NUMERIC) AS total_shipping_price_set_presentment_money_amount
+		, NULL AS total_tax_set_presentment_money_amount
+		, NULL AS total_tax_set_shop_money_amount
+		, NULL AS total_tip_received
+		, NULL AS total_weight
+		, loaded_at AS updated_at
 		, user_id
 		, uuid_ts
-		, AS `value`
+		, CAST(purchase_count_littledata AS STRING) AS `value`
 	FROM historical__littledata
+
+	UNION ALL
+
+	SELECT
+		"legacy" AS source_name
+		, NULL AS admin_graphql_api_id
+		, anonymous_id
+		, NULL AS app_id
+		, NULL AS browser_ip
+		, NULL AS buyer_accepts_marketing
+		, NULL AS cart_token
+		, NULL AS checkout_id
+		, token AS checkout_token
+		, NULL AS client_details_accept_language
+		, NULL AS client_details_browser_ip
+		, NULL AS client_details_user_agent
+		, NULL AS closed_at
+		, NULL AS confirmation_number
+		, NULL AS is_confirmed
+		, email AS contact_email
+		, NULL AS context_integration_name
+		, context_ip
+		, context_library_name
+		, context_library_version
+		, NULL AS context_request_ip
+		, NULL AS context_session_id
+		, NULL AS context_source_id
+		, `type` AS context_source_type
+		, NULL AS context_topic
+		, NULL AS created_at
+		, NULL AS currency
+		, NULL AS customer_locale
+		, NULL AS discount_applications
+		, coupon AS discount_codes
+		, email
+		, NULL AS has_estimated_taxes
+		, `event` AS event_name
+		, event_text
+		, NULL AS financial_status
+		, NULL AS fulfillment_status
+		, NULL AS fulfillments
+		, id AS order_created_id
+		, NULL AS landing_site
+		, NULL AS landing_site_ref
+		, loaded_at
+		, NULL AS location_id
+		, NULL AS `name`
+		, NULL AS note
+		, NULL AS note_attributes
+		, NULL AS `number`
+		, order_id
+		, NULL AS order_number
+		, NULL AS order_status_url
+		, original_timestamp
+		, NULL AS payment_gateway_names
+		, NULL AS payment_terms_created_at
+		, NULL AS payment_terms_id
+		, NULL AS payment_terms_payment_terms_name
+		, NULL AS payment_terms_payment_terms_type
+		, NULL AS payment_terms_updated_at
+		, phone
+		, NULL AS presentment_currency
+		, NULL AS processed_at
+		, products
+		, received_at
+		, NULL AS referring_site
+		, sent_at
+		, shipping_name AS shipping_lines
+		, NULL AS channel
+		, subtotal AS subtotal_price
+		, NULL AS tags
+		, COALESCE(CAST(tax AS NUMERIC), 0) AS tax
+		, NULL AS tax_exempt
+		, NULL AS tax_lines
+		, NULL AS taxes_included
+		, `timestamp`
+		, token
+		, COALESCE(CAST(discount AS NUMERIC), 0) AS total_discounts
+		, COALESCE(CAST(total AS NUMERIC), 0) AS total_line_items_price
+		, NULL AS total_outstanding
+		, NULL AS total_price_set_shop_money_amount
+		, CAST(shipping AS NUMERIC) AS total_shipping_price_set_presentment_money_amount
+		, NULL AS total_tax_set_presentment_money_amount
+		, NULL AS total_tax_set_shop_money_amount
+		, NULL AS total_tip_received
+		, NULL AS total_weight
+		, NULL AS updated_at
+		, user_id
+		, uuid_ts
+		, NULL AS `value`
+	FROM historical__legacy
 )
+
+SELECT * FROM cleaned
+QUALIFY ROW_NUMBER() OVER (PARTITION BY order_created_id ORDER BY loaded_at DESC) = 1
+
--- quip_data/models/stage/rudderstack/stg_rudderstack__tracks.sql
+++ quip_data/models/stage/rudderstack/stg_rudderstack__tracks.sql
@@ -4,21 +4,183 @@ source AS (
 	SELECT * FROM {{ source('rudderstack_prod', 'tracks') }}
 )
 
+, historical__littledata AS (
+	SELECT * FROM {{ source("segment", "littledata__tracks") }}
+)
+
+, historical__legacy AS (
+	SELECT * FROM {{ source("segment", "legacy__tracks") }}
+)
+-------------------------------------------------------
+----------------- FINISH REFERENCES -------------------
+-------------------------------------------------------
 
 SELECT
-	id AS event_id
+	"rudderstack" AS source_name
 	, anonymous_id
-	, user_id AS shopify_user_id
-	, event AS event_name
-	, created_at
-	, updated_at
+	, channel
+	, context_app_name
+	, context_app_namespace
+	, context_app_version
+	, context_campaign_campaign
+	, context_campaign_content
+	, context_campaign_creative
+	, context_campaign_device
+	, context_campaign_id
+	, context_campaign_medium
+	, context_campaign_name
+	, context_campaign_source
+	, context_campaign_term
+	, context_cart_token
+	, context_checkout_token
+	, context_integration_name
+	, context_ip
+	, context_library_name
+	, context_library_version
+	, context_locale
+	, context_order_token
+	, context_page_initial_referrer
+	, context_page_initial_referring_domain
+	, context_page_path
+	, context_page_referrer
+	, context_page_referring_domain
+	, context_page_search
+	, context_page_tab_url
+	, context_page_title
+	, context_page_url
+	, context_request_ip
+	, context_screen_height
+	, context_screen_width
+	, CAST(context_session_id AS STRING) AS context_session_id
+	, context_session_start
+	, context_source_id
+	, context_source_type
+	, context_timezone
+	, context_topic
+	, context_user_agent
+	, `event` AS event_name
+	, event_text
+	, id AS tracks_id
+	, loaded_at
+	, original_timestamp
 	, received_at
 	, sent_at
-	, processed_at
-	, loaded_at AS source_synced_at
-	, original_timestamp
-	, timestamp AS event_timestamp
-	, source_name AS event_source_name
+	, `timestamp`
+	, user_id
+	, uuid_ts
 FROM source
-WHERE NOT test
-QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY loaded_at DESC ) = 1
+
+UNION ALL
+
+SELECT
+	"littledata" AS source_name
+	, anonymous_id
+	, NULL AS channel
+	, NULL AS context_app_name
+	, NULL AS context_app_namespace
+	, NULL AS context_app_version
+	, NULL AS context_campaign_campaign
+	, context_campaign_content
+	, context_campaign_creative
+	, context_campaign_device
+	, context_campaign_id
+	, context_campaign_medium
+	, context_campaign_name
+	, context_campaign_source
+	, context_campaign_term
+	, NULL AS context_cart_token
+	, NULL AS context_checkout_token
+	, context_integration_name
+	, context_ip
+	, context_library_name
+	, context_library_version
+	, context_locale
+	, NULL AS context_order_token
+	, NULL AS context_page_initial_referrer
+	, NULL AS context_page_initial_referring_domain
+	, context_page_path
+	, context_page_referrer
+	, NULL AS context_page_referring_domain
+	, context_page_search
+	, NULL AS context_page_tab_url
+	, context_page_title
+	, context_page_url
+	, NULL AS context_request_ip
+	, CAST(context_screen_height AS INTEGER) AS context_screen_height
+	, CAST(context_screen_width AS INTEGER) AS context_screen_width
+	, context_google_analytics_session_id AS context_session_id
+	, NULL AS context_session_start
+	, NULL AS context_source_id
+	, NULL AS context_source_type
+	, context_timezone
+	, NULL AS context_topic
+	, context_user_agent
+	, `event` AS event_name
+	, event_text
+	, id AS tracks_id
+	, loaded_at
+	, original_timestamp
+	, received_at
+	, sent_at
+	, `timestamp`
+	, user_id
+	, uuid_ts
+FROM historical__littledata
+
+
+UNION ALL
+
+SELECT
+	"legacy" AS source_name
+	, anonymous_id
+	, NULL AS channel
+	, NULL AS context_app_name
+	, NULL AS context_app_namespace
+	, NULL AS context_app_version
+	, context_campaign_capaign AS context_campaign_campaign
+	, context_campaign_content
+	, context_campaign_creative
+	, context_campaign_device
+	, context_campaign_id
+	, context_campaign_medium
+	, context_campaign_name
+	, context_campaign_source
+	, context_campaign_term
+	, NULL AS context_cart_token
+	, NULL AS context_checkout_token
+	, NULL AS context_integration_name
+	, context_ip
+	, context_library_name
+	, context_library_version
+	, context_locale
+	, NULL AS context_order_token
+	, NULL AS context_page_initial_referrer
+	, NULL AS context_page_initial_referring_domain
+	, context_page_path
+	, context_page_referrer
+	, NULL AS context_page_referring_domain
+	, context_page_search
+	, NULL AS context_page_tab_url
+	, context_page_title
+	, context_page_url
+	, NULL AS context_request_ip
+	, NULL AS context_screen_height
+	, NULL AS context_screen_width
+	, NULL AS context_session_id
+	, NULL AS context_session_start
+	, NULL AS context_source_id
+	, NULL AS context_source_type
+	, NULL AS context_timezone
+	, NULL AS context_topic
+	, context_user_agent
+	, `event` AS event_name
+	, event_text
+	, id AS tracks_id
+	, loaded_at
+	, original_timestamp
+	, received_at
+	, sent_at
+	, `timestamp`
+	, user_id
+	, uuid_ts
+FROM historical__legacy
