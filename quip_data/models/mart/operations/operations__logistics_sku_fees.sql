1ef85ec7bee64f1ef533ffefbb6e52a91e98106e
Dipali Mistri <dipalim06@gmail.com>
2025-03-28 09:36:19 -0700

    Update operations__logistics_sku_fees.sql

--- quip_data/models/mart/operations/operations__logistics_sku_fees.sql
+++ quip_data/models/mart/operations/operations__logistics_sku_fees.sql
@@ -14,13 +14,13 @@ fees AS (
 
 
 SELECT
-	house_bill_number
+	{{ dbt_utils.generate_surrogate_key(['house_bill_number', 'sku', 'tariff_type', 'tariff_number']) }} as id
+	, house_bill_number
 	, sku
 	, sku_presentment
 	, 'tariff' AS fee_type
 	, tariff_type AS fee_detail_1
 	, tariff_number AS fee_detail_2
-	, {{ dbt_utils.generate_surrogate_key(['house_bill_number', 'sku', 'tariff_type', 'tariff_number']) }} as row_id
 	, SUM(total_allocated_tariff_cost) AS total_allocated_amount
 FROM tariffs
 WHERE tariff_type = 'hts_china'
@@ -29,13 +29,13 @@ GROUP BY 1,2,3,4,5,6,7
 UNION ALL
 
 SELECT
-	house_bill_number
+	{{ dbt_utils.generate_surrogate_key(['house_bill_number', 'sku', 'tariff_type', 'tariff_number']) }} as id
+	, house_bill_number
 	, sku
 	, sku_presentment
 	, 'tariff' AS fee_type
 	, tariff_type AS fee_detail_1
 	, tariff_number AS fee_detail_2
-	, {{ dbt_utils.generate_surrogate_key(['house_bill_number', 'sku', 'tariff_type', 'tariff_number']) }} as row_id
 	, SUM(total_allocated_tariff_cost) AS total_allocated_amount
 FROM tariffs
 WHERE tariff_type = 'hts'
@@ -44,13 +44,13 @@ GROUP BY 1,2,3,4,5,6,7
 UNION ALL
 
 SELECT
-	house_bill_number
+	{{ dbt_utils.generate_surrogate_key(['house_bill_number', 'sku', 'charge_category', 'charge_code', 'charge_name']) }} as id	
+	, house_bill_number
 	, sku
 	, sku_presentment
 	, charge_category AS fee_type
 	, charge_code AS fee_detail_1
 	, charge_name AS fee_detail_2
-	, {{ dbt_utils.generate_surrogate_key(['house_bill_number', 'sku', 'charge_category', 'charge_code', 'charge_name']) }} as row_id
 	, SUM(allocated_invoice_amount) AS total_allocated_amount
 FROM fees
 GROUP BY 1,2,3,4,5,6,7
