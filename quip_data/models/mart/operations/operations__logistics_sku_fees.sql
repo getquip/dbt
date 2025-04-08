0b484ada07ba531031874ef7ce7ce06645caae4a
Dipali Mistri <dipalim06@gmail.com>
2025-04-08 15:13:06 -0700

    Update operations__logistics_sku_fees.sql

--- quip_data/models/mart/operations/operations__logistics_sku_fees.sql
+++ quip_data/models/mart/operations/operations__logistics_sku_fees.sql
@@ -17,13 +17,14 @@ SELECT
 	{{ dbt_utils.generate_surrogate_key(['house_bill_number', 'sku', 'tariff_type', 'tariff_number']) }} as id
 	, house_bill_number
 	, sku
+  	, REPLACE(sku, 'R', '') AS sku_presentment
 	, 'tariff' AS fee_type
 	, tariff_type AS fee_detail_1
 	, tariff_number AS fee_detail_2
 	, SUM(total_allocated_tariff_cost) AS total_allocated_amount
 FROM tariffs
 WHERE tariff_type = 'hts_china'
-GROUP BY 1,2,3,4,5,6
+GROUP BY 1,2,3,4,5,6,7
 
 UNION ALL
 
@@ -31,13 +32,14 @@ SELECT
 	{{ dbt_utils.generate_surrogate_key(['house_bill_number', 'sku', 'tariff_type', 'tariff_number']) }} as id
 	, house_bill_number
 	, sku
+  	, REPLACE(sku, 'R', '') AS sku_presentment
 	, 'tariff' AS fee_type
 	, tariff_type AS fee_detail_1
 	, tariff_number AS fee_detail_2
 	, SUM(total_allocated_tariff_cost) AS total_allocated_amount
 FROM tariffs
 WHERE tariff_type = 'hts'
-GROUP BY 1,2,3,4,5,6
+GROUP BY 1,2,3,4,5,6,7
 
 UNION ALL
 
@@ -45,9 +47,10 @@ SELECT
 	{{ dbt_utils.generate_surrogate_key(['house_bill_number', 'sku', 'charge_category', 'charge_code', 'charge_name']) }} as id	
 	, house_bill_number
 	, sku
+  	, REPLACE(sku, 'R', '') AS sku_presentment
 	, charge_category AS fee_type
 	, charge_code AS fee_detail_1
 	, charge_name AS fee_detail_2
 	, SUM(allocated_invoice_amount) AS total_allocated_amount
 FROM fees
-GROUP BY 1,2,3,4,5,6
+GROUP BY 1,2,3,4,5,6,7
